18b27ab374c41087a5b6fe7e7b8713b4ff3b3fa1
beppvis <visruthvijind@gmail.com>
2024-08-10 07:11:46 +0530

    Viva la inventory

--- menu.c
+++ menu.c
@@ -1,7 +1,7 @@
 #include "menu.h"
 #include "player.h"
-#include <fenv.h>
 #include <raylib.h>
+#include <stdint.h>
 #include <stdio.h>
 
 void playerMenuHandler(Inventory *inv)
@@ -48,7 +48,8 @@ void renderInventoryMenu(Player *player)
 
     Rectangle inv_rec = getRect(pos,(Size){boxSize,boxSize});
 
-    if (item->itemType != 0 && num+1 != player->inventory.item_grabbed_index)
+    printf("%d , %d\n",num , player->inventory.item_grabbed_index);
+    if (item->itemType != 0 && i != player->inventory.item_grabbed_index)
     {
       Rectangle source_rec = {0,0,item->sprite.width,item->sprite.height};
       DrawTexturePro(item->sprite,source_rec ,inv_rec , (Vector2){0,0}, 0.,WHITE);
@@ -64,15 +65,29 @@ void inventoryMouseInteraction(Player *player,Camera2D *camera)
 {
   Rectangle menuBox = {player->position.x-250,player->position.y-450,500,800};
   Vector2 mousePos = GetScreenToWorld2D( GetMousePosition(),*camera);
+  Inventory *inventory = &player->inventory;
   for (int i =0; i < PLAYER_INV_LEN; i++)
   {
-
-    Item item = player->inventory.MainSlots[i];
+    Item item = inventory->MainSlots[i];
     if (isInside(boxSize, item.inv_pos,mousePos)&&IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
     {
-      player->inventory.item_grabbed_index = i+1;
+      if(inventory->item_grabbed_index != -1)
+      {
+        //To get the item
+
+        Item grabbed_item = inventory->MainSlots[inventory->item_grabbed_index];
+        if (item.itemType!=0)
+        {
+          inventory->MainSlots[inventory->item_grabbed_index] =inventory->MainSlots[i];  
+        }
+        inventory->MainSlots[i] = grabbed_item;
+        inventory->item_grabbed_index = i;
+        return;
+      }
+      inventory->item_grabbed_index = i;
       printf("Mouse pos : (%f,%f)\n",mousePos.x,mousePos.y);
       printf("Grabbed pos (%f,%f)\n",item.inv_pos.x,item.inv_pos.y);
+      return;
     }
 
   }
@@ -81,8 +96,8 @@ void inventoryMouseInteraction(Player *player,Camera2D *camera)
 void ItemMouse(Inventory *inventory,Camera2D camera)
 {
   Vector2 MousePos = GetScreenToWorld2D(GetMousePosition(),camera);
-  if (inventory->item_grabbed_index - 1 == -1){return;}
-  Item item  = inventory->MainSlots[inventory->item_grabbed_index-1];
+  if (inventory->item_grabbed_index  == -1){return;}
+  Item item  = inventory->MainSlots[inventory->item_grabbed_index];
   if (!inventory->menu_open){return;}
   if (item.itemType == 0){return;}
 
